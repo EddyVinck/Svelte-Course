7161a74dce2296771a163dc590571fc7c902e452
EddyVinck <eddyvinck95@gmail.com>
2019-06-12 22:49:54 +0200

    wrap up first course module

---
+++ assignment-1/src/CourseGoal.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let courseGoal = "";
+</script>
+
+<style>
+  .burning-passion {
+    color: red;
+    font-weight: bold;
+  }
+</style>
+
+<h1 class:burning-passion={courseGoal.includes('!')}>{courseGoal}</h1>
