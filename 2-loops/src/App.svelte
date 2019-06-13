079ac99ca1e2e58c54042f0877acff3b180e38db
EddyVinck <eddyvinck95@gmail.com>
2019-06-13 23:13:29 +0200

    conditionals and loops

---
+++ 2-loops/src/App.svelte
@@ -0,0 +1,75 @@
+<script>
+  import ContactCard from "./ContactCard.svelte";
+
+  let name = "Max";
+  let title = "";
+  let image = "";
+  let description = "";
+  let formState = "empty";
+  let createdContacts = [];
+
+  function addContact() {
+    if (
+      name.trim().length == 0 ||
+      title.trim().length == 0 ||
+      image.trim().length == 0 ||
+      description.trim().length == 0
+    ) {
+      formState = "invalid";
+      return;
+    }
+    // createdContacts.push({ will not work because Svelte cannot detect changes to the referenced array, it needs to be reassigned
+    createdContacts = [
+      ...createdContacts,
+      {
+        name,
+        jobTitle: title,
+        imageUrl: image,
+        description
+      }
+    ];
+    formState = "done";
+  }
+</script>
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>Add contact card</button>
+
+{#if formState == 'invalid'}
+  <p>invalid input</p>
+{:else}
+  <p>Please enter some information and click the button</p>
+{/if}
+
+{#each createdContacts as contact}
+  <ContactCard
+    userName={contact.name}
+    jobTitle={contact.jobTitle}
+    description={contact.description}
+    userImage={contact.imageUrl} />
+{/each}
