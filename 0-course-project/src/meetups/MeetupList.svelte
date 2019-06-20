320186165f716e94027ed9fcc2c9111651bc7136
EddyVinck <eddyvinck95@gmail.com>
2019-06-20 23:03:26 +0200

    start the course project, display a list of meetups

---
+++ 0-course-project/src/meetups/MeetupList.svelte
@@ -0,0 +1,25 @@
+<script>
+  import MeetupItem from "./MeetupItem.svelte";
+
+  export let meetups = [];
+</script>
+
+<style>
+  section {
+    margin-top: 5rem;
+  }
+</style>
+
+<section>
+  {#each meetups as meetup (meetup.id)}
+    <MeetupItem
+      title={meetup.title}
+      subtitle={meetup.subtitle}
+      imageUrl={meetup.imageUrl}
+      description={meetup.description}
+      address={meetup.address}
+      email={meetup.contactEmail} />
+  {:else}
+    <p>No meetups available</p>
+  {/each}
+</section>
