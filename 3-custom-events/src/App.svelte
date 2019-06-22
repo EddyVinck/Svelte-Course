55b30b0023070102e25459d7e374d7997ee31604
EddyVinck <eddyvinck95@gmail.com>
2019-06-22 21:52:39 +0200

    emit custom events in Svelte

---
+++ 3-custom-events/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Product from "./Product.svelte";
+</script>
+
+<Product
+  on:add-to-cart={e => alert('add to cart!')}
+  on:delete={e => alert('delete!')}
+  on:click={e => alert('Hello world')}
+  productTitle="A keyboard" />
---
+++ 3-custom-events/src/Product.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let productTitle;
+
+  const dispatch = createEventDispatcher();
+
+  function addToCart() {
+    const productData = {};
+    dispatch("add-to-cart", productData);
+  }
+</script>
+
+<article>
+  <h1>{productTitle}</h1>
+  <!-- Without the handler function, the event is forwarded -->
+  <button on:click={addToCart}>Add to cart</button>
+  <button on:click={() => dispatch('delete', 'id1')}>Delete product</button>
+</article>
