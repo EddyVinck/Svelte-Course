29386b22542ce2f759f870cbd43b72cb66d87312
EddyVinck <eddyvinck95@gmail.com>
2019-06-22 20:30:08 +0200

    add custom form field components to the meetup creation form

--- 0-course-project/src/App.svelte
+++ 0-course-project/src/App.svelte
@@ -1,8 +1,18 @@
 <script>
   import Header from "./ui/Header.svelte";
   import MeetupList from "./meetups/MeetupList.svelte";
+  import TextInput from "./ui/TextInput.svelte";
 
-  const meetups = [
+  let title = "My test meetup";
+  let subtitle = "This is going to be so much fun!";
+  let address = "Nerdstreet 34th";
+  let description =
+    "Listen to some nerds talking about tech and talk to some nerds about that tech afterwards. Also, Pizza! 🍕";
+  let imageUrl =
+    "https://images.pexels.com/photos/34676/pexels-photo.jpg?auto=format%2Ccompress&amp;cs=tinysrgb&amp;h=750&amp;w=1260";
+  let contactEmail = "meetup@test.com";
+
+  let meetups = [
     {
       id: "m1",
       title: "Eindhoven Front-end Meetup: Lightning Talks",
@@ -26,13 +36,82 @@
       contactEmail: "code@test.com"
     }
   ];
+
+  function addMeetup() {
+    //
+    const newMeetup = {
+      id: Math.random().toString(),
+      title,
+      subtitle,
+      description,
+      imageUrl,
+      contactEmail,
+      address
+    };
+
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
 <style>
-
+  form {
+    max-width: 90%;
+    width: 30rem;
+    margin: auto;
+  }
 </style>
 
 <Header />
 <h1>Time for a project!</h1>
 
-<MeetupList {meetups} />
+<main>
+
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      value={title}
+      on:input={event => {
+        title = event.target.value;
+      }} />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      value={subtitle}
+      on:input={event => {
+        subtitle = event.target.value;
+      }} />
+    <TextInput
+      id="address"
+      label="Address"
+      value={address}
+      on:input={event => {
+        address = event.target.value;
+      }} />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      value={imageUrl}
+      on:input={event => {
+        imageUrl = event.target.value;
+      }} />
+    <TextInput
+      id="contactEmail"
+      label="Contact Email"
+      value={contactEmail}
+      on:input={event => {
+        contactEmail = event.target.value;
+      }} />
+    <TextInput
+      controlType="textarea"
+      id="description"
+      label="Description"
+      value={description}
+      on:input={event => {
+        description = event.target.value;
+      }} />
+    <button type="submit">Save</button>
+  </form>
+  <MeetupList {meetups} />
+
+</main>
--- 0-course-project/src/meetups/MeetupList.svelte
+++ 0-course-project/src/meetups/MeetupList.svelte
@@ -8,6 +8,18 @@
   section {
     margin-top: 5rem;
   }
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
 </style>
 
 <section>
