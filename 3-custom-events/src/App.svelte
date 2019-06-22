9aea259749192f097e57677209861a5edd008faa
EddyVinck <eddyvinck95@gmail.com>
2019-06-22 23:01:14 +0200

    add a modal with a slot

--- 3-custom-events/src/App.svelte
+++ 3-custom-events/src/App.svelte
@@ -1,9 +1,27 @@
 <script>
   import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
+
+  export const products = [
+    {
+      id: "1",
+      title: "A keyboard",
+      price: 95.99
+    }
+  ];
+
+  function addToCart(event) {
+    console.log(event.detail); // the event data
+  }
+  function deleteProduct(event) {
+    console.log(event.detail);
+  }
 </script>
 
-<Product
-  on:add-to-cart={e => alert('add to cart!')}
-  on:delete={e => alert('delete!')}
-  on:click={e => alert('Hello world')}
-  productTitle="A keyboard" />
+{#each products as product}
+  <Product on:add-to-cart={addToCart} on:delete={deleteProduct} {...product} />
+{/each}
+
+<Modal>
+  <h1>Hello Modal!</h1>
+</Modal>
--- 3-custom-events/src/Product.svelte
+++ 3-custom-events/src/Product.svelte
@@ -1,17 +1,25 @@
 <script>
   import { createEventDispatcher } from "svelte";
-  export let productTitle;
+  export let title;
+  export let price;
+  export let bestSeller = false;
 
   const dispatch = createEventDispatcher();
-
   function addToCart() {
-    const productData = {};
+    const productData = {
+      id: "lkjsdfkldsj123"
+    };
     dispatch("add-to-cart", productData);
   }
 </script>
 
 <article>
-  <h1>{productTitle}</h1>
+  <h1>{title}</h1>
+  <h2>${price}</h2>
+
+  {#if bestSeller}
+    <h3>Bestseller!!</h3>
+  {/if}
   <!-- Without the handler function, the event is forwarded -->
   <button on:click={addToCart}>Add to cart</button>
   <button on:click={() => dispatch('delete', 'id1')}>Delete product</button>
