268fd5d7e69669d23e1b06740200552eee9faf9e
EddyVinck <eddyvinck95@gmail.com>
2019-06-23 20:00:01 +0200

    various types of form input binding

---
+++ 4-bindings-forms/src/Toggle.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let chosenOption = 1;
+</script>
+
+<button on:click={() => (chosenOption = 1)} disabled={chosenOption === 1}>
+  1
+</button>
+<button on:click={() => (chosenOption = 2)} disabled={chosenOption === 2}>
+  2
+</button>
