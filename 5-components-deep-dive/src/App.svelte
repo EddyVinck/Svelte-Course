b5ed88291de622b78d950ad97cd8906bb6c5d0a8
EddyVinck <eddyvinck95@gmail.com>
2020-04-27 20:00:40 +0200

    component slots and events

---
+++ 5-components-deep-dive/src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  /**
+   * Course section 6
+   */
+  import Product from "./Product.svelte";
+  import Modal from "./Modal.svelte";
+
+  let products = [
+    {
+      id: "my_id1",
+      title: "So Good They Can't ignore you",
+      price: 9.99
+    }
+  ];
+
+  let showModal = false;
+  let closable;
+  function handleAddToCart(event) {
+    console.log(event);
+  }
+  function handleDelete(event) {
+    console.log(event);
+  }
+</script>
+
+<button
+  on:click={() => {
+    showModal = true;
+  }}>
+  Show modal
+</button>
+
+{#if showModal}
+  <Modal on:cancelmodal={() => (showModal = false)} let:didAgree={closable}>
+    <h1>This is my modal!</h1>
+    <button disabled={!closable}>Close it</button>
+  </Modal>
+{/if}
+
+{#each products as product (product.id)}
+  <Product
+    on:addToCart={handleAddToCart}
+    on:deleteProduct={handleDelete}
+    {...product} />
+{/each}
---
+++ 5-components-deep-dive/src/Product.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  export let title = "";
+  export let price = 10;
+  export let isBestseller = false;
+  export let id;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<article>
+  <h1>{title}</h1>
+  <p>${price}</p>
+  {#if isBestseller}
+    <span>BESTSELLER BABYYY</span>
+  {/if}
+  <button on:click={() => dispatch('addToCart', { id })}>Add to cart</button>
+  <button on:click={() => dispatch('deleteProduct', { id })}>Delete</button>
+</article>
