1a4971e79f060a87202378c00a3da5397b2aa477
EddyVinck <eddyvinck95@gmail.com>
2020-04-27 16:10:04 +0200

    move example data to separate file

--- 0-course-project/src/App.svelte
+++ 0-course-project/src/App.svelte
@@ -5,34 +5,11 @@
   import TextInput from "./ui/TextInput.svelte";
   import Button from "./ui/Button.svelte";
 
+  import { exampleMeetups } from "./data/example-meetups.js";
+
   let editMode = null;
 
-  let meetups = [
-    {
-      id: "m1",
-      title: "Eindhoven Front-end Meetup: Lightning Talks",
-      subtitle: "ISAAC",
-      description:
-        "Lightning talks?! Yes! This time our meet-up will take a different approach: Short, powerful lightning talks (max. 10 minutes) and more time (10 - 20 minutes) for questions and conversations.",
-      imageUrl:
-        "https://images.pexels.com/photos/34676/pexels-photo.jpg?auto=format%2Ccompress&cs=tinysrgb&h=750&w=1260",
-      address: "Marconilaan 16, Eindhoven",
-      contactEmail: "code@test.com",
-      isFavorite: false
-    },
-    {
-      id: "m2",
-      title: "A Refreshing Frontend Meetup!",
-      subtitle: "Frontmen",
-      description:
-        "Eindhoven.js is hosting this evening with great talks about MagicMove by Hein Rutjes (ReactEurope preview!) and Insights on GraphQL by Albert Groothedde.",
-      imageUrl:
-        "https://images.pexels.com/photos/34676/pexels-photo.jpg?auto=format%2Ccompress&cs=tinysrgb&h=750&w=1260",
-      address: "Kick Offices, Eindhoven",
-      contactEmail: "code@test.com",
-      isFavorite: false
-    }
-  ];
+  let meetups = [...exampleMeetups];
 
   function addMeetup({ detail }) {
     console.log(event);
---
+++ 0-course-project/src/data/example-meetups.js
@@ -0,0 +1,26 @@
+export const exampleMeetups = [
+  {
+    id: "m1",
+    title: "Eindhoven Front-end Meetup: Lightning Talks",
+    subtitle: "ISAAC",
+    description:
+      "Lightning talks?! Yes! This time our meet-up will take a different approach: Short, powerful lightning talks (max. 10 minutes) and more time (10 - 20 minutes) for questions and conversations.",
+    imageUrl:
+      "https://images.pexels.com/photos/34676/pexels-photo.jpg?auto=format%2Ccompress&cs=tinysrgb&h=750&w=1260",
+    address: "Marconilaan 16, Eindhoven",
+    contactEmail: "code@test.com",
+    isFavorite: false,
+  },
+  {
+    id: "m2",
+    title: "A Refreshing Frontend Meetup!",
+    subtitle: "Frontmen",
+    description:
+      "Eindhoven.js is hosting this evening with great talks about MagicMove by Hein Rutjes (ReactEurope preview!) and Insights on GraphQL by Albert Groothedde.",
+    imageUrl:
+      "https://images.pexels.com/photos/34676/pexels-photo.jpg?auto=format%2Ccompress&cs=tinysrgb&h=750&w=1260",
+    address: "Kick Offices, Eindhoven",
+    contactEmail: "code@test.com",
+    isFavorite: false,
+  },
+];
